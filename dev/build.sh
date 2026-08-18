008cd348376bc3df4776594db894fe31faba874e
cjw <3117132432@qq.com>
2026-08-18 16:14:14 +0800

    feat-私有品牌insider构建支持

--- dev/build.sh
+++ dev/build.sh
@@ -23,8 +23,8 @@ export VSCODE_SKIP_NODE_VERSION_CHECK="yes"
 while getopts ":ilops" opt; do
   case "$opt" in
     i)
-      export ASSETS_REPOSITORY="VSCodium/vscodium-insiders"
-      export BINARY_NAME="codium-insiders"
+      export ASSETS_REPOSITORY="example/kianggo"
+      export BINARY_NAME="kianggo-insiders"
       export VSCODE_QUALITY="insider"
       ;;
     l)
